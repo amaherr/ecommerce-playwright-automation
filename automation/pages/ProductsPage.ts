e84582d28e3dab25caa6b63c384bc8cbd88829b2
Maher1506 <a.maher1506@gmail.com>
2026-08-30 14:03:04 +0300

    Finished writing filtering tests

--- automation/pages/ProductsPage.ts
+++ automation/pages/ProductsPage.ts
@@ -3,18 +3,22 @@ import { type Page, type Locator } from '@playwright/test';
 export class ProductsPage {
     readonly page: Page;
 
-    readonly allProducts: Locator;
+    readonly products: Locator;
     readonly searchbarInput: Locator;
     readonly searchbarButton: Locator;
+    readonly categoryFilter: Locator;
+    readonly priceFilter: Locator;
 
     readonly url: string;
 
     constructor(page: Page) {
         this.page = page;
 
-        this.allProducts = this.page.locator('.product-card');
+        this.products = this.page.locator('.product-card');
         this.searchbarInput = this.page.getByPlaceholder('Search products');
         this.searchbarButton = this.page.locator('#searchBtn');
+        this.categoryFilter = this.page.getByLabel('Category');
+        this.priceFilter = this.page.getByLabel('Max Price');
 
         this.url = '/';
     }
@@ -26,7 +30,13 @@ export class ProductsPage {
     async search(searchWord: string) {
         await this.searchbarInput.fill(searchWord);
         await this.searchbarButton.click();
+    }
 
-        return this.page.locator('.product-card');
+    async filterByCategory(category: string) {
+        await this.categoryFilter.selectOption(category);
+    }
+
+    async filterByMaxPrice(maxPrice: number) {
+        await this.priceFilter.fill(String(maxPrice));
     }
 }
--- automation/tests/products.spec.ts
+++ automation/tests/products.spec.ts
@@ -2,13 +2,18 @@ import { test, expect } from '@playwright/test';
 
 import { ProductsPage } from '../pages/ProductsPage';
 
+////// Helpers //////
+function getPrice(price: string) {
+    return Number(price.replace('$', ''));
+}
+
 test('product cards are displayed correctly', async ({ page }) => {
     const productsPage: ProductsPage = new ProductsPage(page);
 
     await productsPage.goto();
 
     // get all product cards
-    const products = productsPage.allProducts;
+    const products = productsPage.products;
 
     // make sure there are more than 1 product displaying
     const productsCount = await products.count();
@@ -48,15 +53,17 @@ test.describe('searchbar works correctly', async () => {
     test('searchbar finds existing product(s) with exact search', async ({ page }) => {
         // search for an existing product
         const searchString = 'Mechanical Keyboard';
-        const results = await productsPage.search(searchString);
+        await productsPage.search(searchString);
+
+        const products = productsPage.products;
 
         // make sure there is at least one result
-        await expect.poll(async () => results.count()).toBeGreaterThan(0);
+        await expect.poll(async () => products.count()).toBeGreaterThan(0);
 
         // make sure that all products include the search string
-        const count = await results.count();
+        const count = await products.count();
         for (let i = 0; i < count; i++) {
-            const product = results.nth(i);
+            const product = products.nth(i);
 
             await expect(product.getByRole('heading')).toContainText(searchString, {
                 ignoreCase: true,
@@ -66,10 +73,11 @@ test.describe('searchbar works correctly', async () => {
 
     test('searchbar does not find nonexistent products', async ({ page }) => {
         // search for a nonexistent product
-        const results = await productsPage.search('abcd123');
+        await productsPage.search('abcd123');
+        const products = productsPage.products;
 
         // verify no products appear
-        await expect(results).toHaveCount(0);
+        await expect(products).toHaveCount(0);
 
         await expect(page.getByText('No products found matching your criteria.')).toBeVisible();
     });
@@ -77,19 +85,103 @@ test.describe('searchbar works correctly', async () => {
     test('searchbar finds existing product(s) with partial search', async ({ page }) => {
         // search for an existing product
         const searchString = 'Keyboard';
-        const results = await productsPage.search(searchString);
+        await productsPage.search(searchString);
+
+        const products = productsPage.products;
 
         // make sure there is at least one result
-        await expect.poll(async () => results.count()).toBeGreaterThan(0);
+        await expect.poll(async () => products.count()).toBeGreaterThan(0);
 
         // make sure that all products include the search string
-        const count = await results.count();
+        const count = await products.count();
         for (let i = 0; i < count; i++) {
-            const product = results.nth(i);
+            const product = products.nth(i);
 
             await expect(product.getByRole('heading')).toContainText(searchString, {
                 ignoreCase: true,
             });
+        }
+    });
+});
+
+test.describe('filtering and sorting wotks correctly', async () => {
+    let productsPage: ProductsPage;
+
+    // create a new products page object model before each test
+    test.beforeEach(async ({ page }) => {
+        console.log(`Running ${test.info().title}`);
+
+        productsPage = new ProductsPage(page);
+        await productsPage.goto();
+    });
+
+    test('filtering by category works correctly', async ({ page }) => {
+        // filter by electronics
+        const category = 'Electronics';
+        await productsPage.filterByCategory(category);
+
+        const products = productsPage.products;
+
+        // make sure there are no empty results
+        await expect.poll(async () => products.count()).toBeGreaterThan(0);
+
+        // make sure all products returned are within the correct category
+        const count = await products.count();
+        for (let i = 0; i < count; i++) {
+            const product = products.nth(i);
+
+            await expect(product.locator('.product-category')).toHaveText(category, {
+                ignoreCase: true,
+            });
+        }
+    });
+
+    test('filtering by max price works correctly', async ({ page }) => {
+        // filter by max price
+        const maxPrice = 70;
+        await productsPage.filterByMaxPrice(maxPrice);
+
+        const products = productsPage.products;
+
+        // make sure there are results
+        await expect.poll(async () => products.count()).toBeGreaterThan(0);
+
+        // make sure all products have a correct price
+        const count = await products.count();
+        for (let i = 0; i < count; i++) {
+            const product = products.nth(i);
+
+            const price = await product.locator('.product-price').textContent();
+            expect(getPrice(price || '')).toBeLessThanOrEqual(maxPrice);
+        }
+    });
+
+    test('filtering by max price and category works correctly', async ({ page }) => {
+        // filter by max price and category
+        const maxPrice = 30;
+        const category = 'Accessories';
+
+        await productsPage.filterByCategory(category);
+        await productsPage.filterByMaxPrice(maxPrice);
+
+        const products = productsPage.products;
+
+        // make sure results appear
+        await expect.poll(async () => products.count()).toBeGreaterThan(0);
+
+        // make sure every product mathces the filters
+        const count = await products.count();
+        for (let i = 0; i < count; i++) {
+            const product = products.nth(i);
+
+            // category verification
+            await expect(product.locator('.product-category')).toHaveText(category, {
+                ignoreCase: true,
+            });
+
+            // price verification
+            const price = await product.locator('.product-price').textContent();
+            expect(getPrice(price || '')).toBeLessThanOrEqual(maxPrice);
         }
     });
 });
